eb573a7d8912737e7fcbb6b420fd43996fefc25d
Tej MH <tejhagargigit@gmail.com>
2024-07-30 15:08:06 +0530

    .

--- frontend/src/Components/Footer.jsx
+++ frontend/src/Components/Footer.jsx
@@ -1,27 +1,29 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Container from '@mui/material/Container';
-import IconButton from '@mui/material/IconButton';
-import Link from '@mui/material/Link';
-import Stack from '@mui/material/Stack';
-import TextField from '@mui/material/TextField';
-import Typography from '@mui/material/Typography';
 
-import FacebookIcon from '@mui/icons-material/GitHub';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import TwitterIcon from '@mui/icons-material/X';
+
+
+
+import * as React from "react";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import Link from "@mui/material/Link";
+import TextField from "@mui/material/TextField";
+
+import FacebookIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import TwitterIcon from "@mui/icons-material/X";
+import "../Styles/footer.css";
+import { Box, Container, Stack, Typography } from "@mui/material";
 
 const logoStyle = {
-  width: '140px',
-  height: 'auto',
+  width: "70px",
+  marginBottom: "1vw",
+  height: "auto",
 };
 
 function Copyright() {
   return (
     <Typography variant="body2" color="text.secondary" mt={1}>
-      {'Copyright © '}
-      <Link href="https://mui.com/">Sitemark&nbsp;</Link>
+      Copyright © NITAI INNOVATIONS&nbsp;
       {new Date().getFullYear()}
     </Typography>
   );
@@ -31,45 +33,49 @@ export default function Footer() {
   return (
     <Container
       sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
         gap: { xs: 4, sm: 8 },
         py: { xs: 8, sm: 10 },
-        textAlign: { sm: 'center', md: 'left' },
+        textAlign: { xs: "center", sm: "left" },
       }}
     >
       <Box
         sx={{
-          display: 'flex',
-          flexDirection: { xs: 'column', sm: 'row' },
-          width: '100%',
-          justifyContent: 'space-between',
+          display: "flex",
+          flexDirection: { xs: "column", sm: "row" },
+          width: "100%",
+          justifyContent: "space-between",
+          flexWrap: "wrap",
         }}
       >
         <Box
           sx={{
-            display: 'flex',
-            flexDirection: 'column',
+            display: "flex",
+            flexDirection: "column",
             gap: 4,
-            minWidth: { xs: '100%', sm: '60%' },
+            minWidth: { xs: "100%", sm: "40%" },
+            mb: { xs: 4, sm: 0 },
           }}
         >
-          <Box sx={{ width: { xs: '100%', sm: '60%' } }}>
-            <Box sx={{ ml: '-15px' }}>
+          <Box sx={{ width: { xs: "100%", sm: "60%" } }}>
+            <Box sx={{ ml: { xs: 0, sm: "-15px" } }}>
               <img
                 src={
-                  'https://assets-global.website-files.com/61ed56ae9da9fd7e0ef0a967/61f12e6faf73568658154dae_SitemarkDefault.svg'
+                  "public/images/nitaiLogo.png"
                 }
                 style={logoStyle}
                 alt="logo of sitemark"
               />
             </Box>
             <Typography variant="body2" fontWeight={600} gutterBottom>
-              Newsletter
+              ABOUT NITAI INNOVATIONS
             </Typography>
             <Typography variant="body2" color="text.secondary" mb={2}>
-              Subscribe to our newsletter for weekly updates and promotions.
+              We are a passionate and dedicated team of professionals with a
+              mission to make a difference in the world of Information
+              Technology
             </Typography>
             <Stack direction="row" spacing={1} useFlexGap>
               <TextField
@@ -81,11 +87,15 @@ export default function Footer() {
                 aria-label="Enter your email address"
                 placeholder="Your email address"
                 inputProps={{
-                  autoComplete: 'off',
-                  'aria-label': 'Enter your email address',
+                  autoComplete: "off",
+                  "aria-label": "Enter your email address",
                 }}
               />
-              <Button variant="contained" color="primary" sx={{ flexShrink: 0 }}>
+              <Button
+                variant="contained"
+                color="primary"
+                sx={{ flexShrink: 0 }}
+              >
                 Subscribe
               </Button>
             </Stack>
@@ -93,127 +103,125 @@ export default function Footer() {
         </Box>
         <Box
           sx={{
-            display: { xs: 'none', sm: 'flex' },
-            flexDirection: 'column',
+            display: "flex",
+            flexDirection: "column",
             gap: 1,
+            marginRight: { xs: 0, sm: "5rem" },
+            mb: { xs: 4, sm: 0 },
           }}
         >
-          <Typography variant="body2" fontWeight={600}>
-            Product
+          <Typography variant="h5" marginTop={7} color="#0959AA">
+            COMPANY OFFICE
           </Typography>
-          <Link color="text.secondary" href="#">
-            Features
-          </Link>
-          <Link color="text.secondary" href="#">
-            Testimonials
-          </Link>
-          <Link color="text.secondary" href="#">
-            Highlights
-          </Link>
-          <Link color="text.secondary" href="#">
-            Pricing
-          </Link>
-          <Link color="text.secondary" href="#">
-            FAQs
-          </Link>
+          <Box
+            sx={{
+              display: "flex",
+              gap: "1vw",
+              alignItems: "center",
+              mt: 2,
+            }}
+          >
+            <img
+              style={{ width: "20px", height: "20px" }}
+              src="public/images/location.png"
+              alt=""
+            />
+            <Typography variant="body2" color="text.secondary" fontWeight={600}>
+              Ramnagar, 1st main, 7th cross,odugoudar building, Dharwad 580001
+              KA, INDIA.
+            </Typography>
+            
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              gap: "1vw",
+              alignItems: "center",
+            }}
+          >
+            <img
+              style={{ width: "20px", height: "20px" }}
+              src="public/images/gmail.png"
+              alt=""
+            />
+            <Typography variant="body2" color="text.secondary" fontWeight={600}>
+              <a href="mailto:hr@nitaiinnovations.com">
+                <p style={{ textDecoration: "none", textTransform: "none" }}>
+                  hr@nitaiinnovations.com
+                </p>
+              </a>
+            </Typography>
+          </Box>
         </Box>
         <Box
           sx={{
-            display: { xs: 'none', sm: 'flex' },
-            flexDirection: 'column',
+            display: "flex",
+            flexDirection: "column",
             gap: 1,
           }}
         >
-          <Typography variant="body2" fontWeight={600}>
-            Company
+          <Typography variant="h5" marginTop={7} color="#0959AA" sx={{ textAlign: "center" }}>
+            Business Hours
           </Typography>
-          <Link color="text.secondary" href="#">
-            About us
-          </Link>
-          <Link color="text.secondary" href="#">
-            Careers
-          </Link>
-          <Link color="text.secondary" href="#">
-            Press
-          </Link>
-        </Box>
-        <Box
-          sx={{
-            display: { xs: 'none', sm: 'flex' },
-            flexDirection: 'column',
-            gap: 1,
-          }}
-        >
-          <Typography variant="body2" fontWeight={600}>
-            Legal
+          <Typography
+            variant="body2"
+            color="text.secondary"
+            fontWeight={600}
+            sx={{ textAlign: "center" }}
+          >
+            Our support available to help you 24 hours a day, seven days a week.
           </Typography>
-          <Link color="text.secondary" href="#">
-            Terms
-          </Link>
-          <Link color="text.secondary" href="#">
-            Privacy
-          </Link>
-          <Link color="text.secondary" href="#">
-            Contact
-          </Link>
+          <Typography
+            variant="body2"
+            color="text.secondary"
+            fontWeight={600}
+            sx={{ textAlign: "center", mt: 2 }}
+          >
+            <div className="divide">
+              <p>Monday-Friday:</p>
+              <p>9am to 5pm</p>
+            </div>
+          </Typography>
+          <Typography
+            variant="body2"
+            color="text.secondary"
+            fontWeight={600}
+            sx={{ textAlign: "center" }}
+          >
+            <div className="divide">
+              <p>Saturday:</p>
+              <p>10am to 2pm</p>
+            </div>
+          </Typography>
+          <Typography
+            variant="body2"
+            color="text.secondary"
+            fontWeight={600}
+            sx={{ textAlign: "center" }}
+          >
+            <div className="divide">
+              <p>Sunday:</p>
+              <p>Closed</p>
+            </div>
+          </Typography>
         </Box>
       </Box>
       <Box
         sx={{
-          display: 'flex',
-          justifyContent: 'space-between',
+          display: "flex",
+          justifyContent: "space-between",
           pt: { xs: 4, sm: 8 },
-          width: '100%',
-          borderTop: '1px solid',
-          borderColor: 'divider',
+          width: "100%",
+          borderTop: "1px solid",
+          borderColor: "divider",
         }}
       >
         <div>
           <Link color="text.secondary" href="#">
             Privacy Policy
           </Link>
-          <Typography display="inline" sx={{ mx: 0.5, opacity: 0.5 }}>
-            &nbsp;•&nbsp;
-          </Typography>
-          <Link color="text.secondary" href="#">
-            Terms of Service
-          </Link>
           <Copyright />
         </div>
-        <Stack
-          direction="row"
-          justifyContent="left"
-          spacing={1}
-          useFlexGap
-          sx={{
-            color: 'text.secondary',
-          }}
-        >
-          <IconButton
-            color="inherit"
-            href="https://github.com/mui"
-            aria-label="GitHub"
-            sx={{ alignSelf: 'center' }}
-          >
-            <FacebookIcon />
-          </IconButton>
-          <IconButton
-            color="inherit"
-            href="https://x.com/MaterialUI"
-            aria-label="X"
-            sx={{ alignSelf: 'center' }}
-          >
-            <TwitterIcon />
-          </IconButton>
-          <IconButton
-            color="inherit"
-            href="https://www.linkedin.com/company/mui/"
-            aria-label="LinkedIn"
-            sx={{ alignSelf: 'center' }}
-          >
-            <LinkedInIcon />
-          </IconButton>
-        </Stack>
       </Box>
     </Container>
   );
