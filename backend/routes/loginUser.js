67a9eee895c3534604adcec6b52eee59bbdc4fec
Tej MH <tejhagargigit@gmail.com>
2024-08-03 20:52:04 +0530

    implemented password hashing

--- backend/routes/loginUser.js
+++ backend/routes/loginUser.js
@@ -2,22 +2,29 @@ const express = require("express");
 const router = express.Router();
 const Users = require("../models/users");
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 router.post("/", async (req, res) => {
   try {
     const { email, password } = req.body;
-    const user = await Users.findOne({ email, password });
+    const user = await Users.findOne({ email });
 
     if (user) {
-      const token = jwt.sign(
-        {
-          name: user.name,
-          email: user.email,
-        },
-        "#secretKey123"
-      );
-
-      return res.json({ status: "ok", user: token });
+      const match = await bcrypt.compare(password, user.password);
+      if (match) {
+        const token = jwt.sign(
+          {
+            name: user.name,
+            email: user.email,
+          },
+          "#secretKey123"
+        );
+        res.status(200).send("Login successful");
+      } else {
+        res.status(400).send("Invalid username or password");
+      }
+    } else {
+      res.status(400).send("User not found");
     }
   } catch (err) {
     console.log(err);
--- backend/routes/registerUser.js
+++ backend/routes/registerUser.js
@@ -1,17 +1,22 @@
 const express = require("express");
 const router = express.Router();
 const Users = require("../models/users");
+const bcrypt = require("bcrypt");
 
 router.post("/", async (req, res) => {
   const { username, firstName, lastName, email, password } = req.body;
   try {
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+
     const user = await Users.create({
       username,
       firstName,
       lastName,
       email,
-      password,
+      password: hashedPassword,
     });
+
     res.send(user);
   } catch (err) {
     console.log(err);
