4c230b97d9408b193cc5b6740079c019f3840694
Ganesh <ganeshmk247@gmail.com>
2024-08-22 14:15:42 +0530

    e

--- backend/api/index.js
+++ backend/api/index.js
@@ -7,7 +7,6 @@ require("dotenv").config();
 // Import routes
 const registerUser = require("../routes/registerUser");
 const loginUser = require("../routes/loginUser");
-const mainPage = require("../routes/mainPage"); // Not used, consider removing
 const allUser = require("../routes/allUser");
 const createBlog = require("../routes/createBlog");
 const clearAllUser = require("../routes/clearAllUser");
@@ -21,15 +20,15 @@ const deleteBlog = require("../routes/deleteBlog");
 const app = express();
 
 // CORS configuration
-const corsOptions = {
-  origin: ["https://nitaiinnovations.vercel.app", "http://localhost:5173"],
-  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-  allowedHeaders: ["Content-Type", "Authorization"], // Specify headers you expect
-};
+// const corsOptions = {
+//   origin: ["https://nitaiinnovations.vercel.app", "http://localhost:5173"],
+//   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+//   allowedHeaders: ["Content-Type", "Authorization"], // Specify headers you expect
+// };
 
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
-app.use(cors(corsOptions));
-app.options("*", cors(corsOptions)); // Handle preflight requests for all routes
+// app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+// app.use(cors(corsOptions));
+// app.options("*", cors(corsOptions)); // Handle preflight requests for all routes
 
 // Middleware
 app.use(express.json());
@@ -40,12 +39,12 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 // Connect to MongoDB
-mongoose
-  .connect(
-    process.env.MONGODB_URL || "mongodb://127.0.0.1:27017/nitaiInnovationsDB"
-  )
-  .then(() => console.log("Connected to MongoDB"))
-  .catch((err) => console.log("Not Connected to MongoDB: ", err));
+// mongoose
+//   .connect(
+//     process.env.MONGODB_URL || "mongodb://127.0.0.1:27017/nitaiInnovationsDB"
+//   )
+//   .then(() => console.log("Connected to MongoDB"))
+//   .catch((err) => console.log("Not Connected to MongoDB: ", err));
 
 // Routes
 app.get("/", (req, res) => {
