b407c69614f30163691e98f22e641ef53d856650
Ganesh <ganeshmk247@gmail.com>
2024-08-26 19:00:48 +0530

    children tag error solved

--- frontend/src/Pages/CyberSecurity.jsx
+++ frontend/src/Pages/CyberSecurity.jsx
@@ -24,19 +24,15 @@ function TabPanel(props) {
   const { children, value, index, ...other } = props;
 
   return (
-    <div
+    <Box
       role="tabpanel"
       hidden={value !== index}
-      id={`vertical-tabpanel-${index}`}
-      aria-labelledby={`vertical-tab-${index}`}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
       {...other}
     >
-      {value === index && (
-        <Box sx={{ p: 3 }}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
+      {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
+    </Box>
   );
 }
 
