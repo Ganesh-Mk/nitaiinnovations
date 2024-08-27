1b4d0d846d99bf128a95e92935a07009f5d300c8
Ganesh <ganeshmk247@gmail.com>
2024-08-27 21:36:53 +0530

    now it should work

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "build", //  Ensure this matches your Netlify publish directory
+    outDir: "build",
+    rollupOptions: {
+      input: "/index.html",
+    },
   },
 });
