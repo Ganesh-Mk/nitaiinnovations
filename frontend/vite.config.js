5d7206916fd4990b9d035ea20fe10a56c00f3771
Ganesh <ganeshmk247@gmail.com>
2024-08-27 21:12:55 +0530

    vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "build", // Ensure this matches your Netlify publish directory
+    outDir: "build", //  Ensure this matches your Netlify publish directory
   },
 });
